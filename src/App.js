5df573421c8caa8afd4e745c830bd5cf0d905092
“Catherine <catherineamf@gmail.com>
2023-03-17 19:13:28 +0200

    Fixed relative root

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 import { Characters } from './pages/Characters';
 import { Character } from './pages/Character';
 import { Notfoundpage } from './pages/Notfoundpage';
-import { Layout } from '../components/Layout';
+import { Layout } from './components/Layout';
 function App() {
   return (
     <Routes>
